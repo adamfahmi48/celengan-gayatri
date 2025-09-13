36dbd121450119de7bf564ac9a45d6868324542b
adamfahmi48 <adamfahmifandisyah@gmail.com>
2025-09-13 11:53:46 +0700

    update_v4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1093,16 +1093,8 @@ export default function App() {
 
     // --- AUTO LOGOUT KARENA TIDAK ADA AKTIVITAS ---
   const timerRef = useRef(null);
-  // const INACTIVITY_MS = 5 * 60 * 1000; // 5 menit
-  const INACTIVITY_MS = 5 * 1000; // 5 menit
-
-  // const scheduleLogout = () => {
-  //   if (timerRef.current) clearTimeout(timerRef.current);
-  //   timerRef.current = setTimeout(() => {
-  //     alert("Anda otomatis keluar karena tidak ada aktivitas selama 5 menit.");
-  //     handleLogout();
-  //   }, INACTIVITY_MS);
-  // };
+  const INACTIVITY_MS = 5 * 60 * 1000; // 5 menit
+  // const INACTIVITY_MS = 5 * 1000; // 5 menit
 
   const scheduleLogout = () => {
     if (timerRef.current) clearTimeout(timerRef.current);
@@ -1113,8 +1105,6 @@ export default function App() {
       handleLogout();
     }, INACTIVITY_MS);
   };
-
-  
 
   useEffect(() => {
     if (!currentUser) return;
